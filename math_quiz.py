f093c7caffcfd5e591835bf74d98f39574f3614f
Shreyas raghavan <shreyas3108@gmail.com>
2018-04-27 20:40:26 +0530

    Update math_quiz.py

--- math_quiz.py
+++ math_quiz.py
@@ -5,7 +5,7 @@ months ={1:'January', 2: 'February', 3:'March', 4:'April', 5 :'May', 6: 'June', 
         10:'October', 11 :'November', 12:'December'}
 
 month =input("Please choose wich month (1-12) you would like more information on and type'months' for all months, ex 1 = January\n")
-if month in months.items():
+if month in i for i in months:
     print('thank you for your input')
 
 else:
